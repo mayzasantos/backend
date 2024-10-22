d323996d83a219c39c63875b50d82b4a28db84b8
developer.mayza <developer.mayza@gmail.com>
2024-10-22 17:00:11 -0300

    import sellermodule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { SellersModule } from '../sellers/sellers.module';
 
 
 @Module({
   imports: [
     UsersModule,
+    SellersModule,
     JwtModule.register({
       global: true,
       secret: process.env.SECRET_KEY,
